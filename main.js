6dff02c83ecadf577d8dbbf015b0020c1d7e5696
jmaldonado81 <jojimaldonado@gmail.com>
2020-05-04 23:55:23 -0700

    change

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-function myFunction() {
-    var inpObj = document.getElementById("id1");
-    if (!inpObj.checkValidity()) {
-      document.getElementById("validate").innerHTML = inpObj.validationMessage;
-    } else {
-      document.getElementById("validate").innerHTML = "Input OK";
-    } 
+function
+  primeNumber(){
+    var n, i, flag = true;
+    n = document.myform.n.value; 
+    n = parseInt(n) 
+    for(i = 2; i <= n - 1; i++) 
+        if (n % i == 0) { 
+            flag = false; 
+            break; 
+        } 
+    if (flag == true) 
+      alert(n + ""); 
+    else
+      alert(n + " invalid input please try again"); 
   } 
